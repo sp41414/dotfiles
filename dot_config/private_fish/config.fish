26c7182bf5a10909a9f1d9d46d592e4d2491df10
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-01-18 23:32:20 +0400

    feat: new i3wm + fedora distro setup

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,52 @@
+function fish_prompt -d "Write out the prompt"
+    printf '%s@%s %s%s%s > ' $USER $hostname (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive
+    set fish_greeting
+end
+
+starship init fish | source
+
+if status is-interactive; and not set -q TMUX; and not set -q SSH_TTY
+    exec tmux new-session -A -s main
+end
+
+set PATH "$PATH":"$HOME/.config/scripts"
+bind \cf tmux-sessionizer
+bind \ct 'sudo block'
+
+alias ls 'eza --icons'
+alias clear "printf '\033[2J\033[3J\033[1;1H'"
+function vim
+    if count $argv >/dev/null
+        nvim $argv
+    else
+        nvim .
+    end
+end
+alias venv "source .venv/bin/activate.fish"
+alias src "source $HOME/.config/fish/config.fish"
+
+set -gx EDITOR nvim
+
+function cd
+    builtin cd $argv
+    eza --icons
+end
+
+function mkcd
+    mkdir -p $argv[1]
+    cd $argv[1]
+end
+
+function normalize
+    ffmpeg -i $argv[1] -af loudnorm=I=-14:TP=-1.0:LRA=11 -c:v copy -c:a aac -b:a 192k output.mp4
+end
+
+set -x RIPGREP_CONFIG_PATH /Users/$USER/.config/ripgrep/rgrc
+set -x MANPAGER "nvim +Man!"
+set -x NODE_ENV development
+
+fish_add_path $HOME/go/bin/
+fish_add_path $HOME/.cargo/bin/
