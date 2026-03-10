844e4efb86928424b1783fac8a4a2f00ba73ba13
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-03-10 11:33:41 +0400

    feat: trying out cachyos + hyprland

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,12 +8,12 @@ end
 
 starship init fish | source
 
-set PATH "$PATH":"$HOME/.config/scripts"
+fish_add_path $HOME/.config/scripts
 bind \cf tmux-sessionizer
-bind \ct 'sudo block'
 
 alias ls 'eza --icons'
 alias clear "printf '\033[2J\033[3J\033[1;1H'"
+
 function vim
     if count $argv >/dev/null
         nvim $argv
@@ -21,6 +21,7 @@ function vim
         nvim .
     end
 end
+
 alias venv "source .venv/bin/activate.fish"
 alias src "source $HOME/.config/fish/config.fish"
 
@@ -40,12 +41,9 @@ function normalize
     ffmpeg -i $argv[1] -af loudnorm=I=-14:TP=-1.0:LRA=11 -c:v copy -c:a aac -b:a 192k output.mp4
 end
 
-set -x RIPGREP_CONFIG_PATH /Users/$USER/.config/ripgrep/rgrc
-set -x MANPAGER "nvim +Man!"
+set -gx nvm_default_version lts
 set -x NODE_ENV development
+set -x MANPAGER "nvim +Man!"
 
 fish_add_path $HOME/go/bin/
 fish_add_path $HOME/.cargo/bin/
-
-# opencode
-fish_add_path /home/fedora/.opencode/bin
